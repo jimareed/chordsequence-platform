cdea9e617a153939fbd16cb2bcaebfdee3d332a1
jimareed <jim.a.reed@gmail.com>
2015-06-06 15:19:54 +0000

    updated refresh

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,7 @@
 mkdir /projects
 mkdir /projects/chordsequence
+git pull
 cp -ur ./data /projects/chordsequence/
+chmod -R 777 /projects/chordsequence/data/
+cp -ur ./template /projects/chordsequence/
+cp -ur ./webapps /var/lib/tomcat8
