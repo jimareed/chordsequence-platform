ad9506fb8e25c4619148ca3a805d70eff4915f90
jimareed <jim.a.reed@gmail.com>
2015-06-06 15:21:49 +0000

    updated refresh

--- refresh.sh
+++ refresh.sh
@@ -5,3 +5,4 @@ cp -ur ./data /projects/chordsequence/
 chmod -R 777 /projects/chordsequence/data/
 cp -ur ./template /projects/chordsequence/
 cp -ur ./webapps /var/lib/tomcat8
+service tomcat8 restart
